3f82aa766eeb9f23699cb3d0f8e6cd7b205f550a
Immortian <andmoz2002@yandex.ru>
2022-09-02 09:43:37 +0300

    .NET refactoring

---
+++ FileManager/CreateDatabase.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE IF NOT EXISTS public."History"
+(
+    Id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    Filename character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    Date_visited timestamp without time zone NOT NULL,
+    CONSTRAINT "History_pkey" PRIMARY KEY (Id)
+);
